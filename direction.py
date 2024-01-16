ad18d6b46da89a56941a6c3a4a1c792183d1b782
Xavier Juvigny <juvigny@onera.fr>
2024-01-16 10:23:45 +0100

    pep8 conformation

--- direction.py
+++ direction.py
@@ -1,11 +1,10 @@
 """
-Constantes définissant l'ordonnancement des directions/
-Some constantes for directions
+Some constants for directions
 """
 
 # Directions : Pour avoir la direction opposée, il suffit d'évaluer 3 - dir
-DIR_NONE  = -1 # Pour le début, aucune direction privilégiée...
-DIR_NORTH = 0
-DIR_EAST  = 1
-DIR_WEST  = 2
-DIR_SOUTH = 3
+DIR_NONE  = -1  # Pour le début, aucune direction privilégiée...
+DIR_NORTH =  0
+DIR_EAST  =  1
+DIR_WEST  =  2
+DIR_SOUTH =  3
--- pheromone.py
+++ pheromone.py
@@ -4,31 +4,34 @@ import numpy as np
 import direction as d
 import pygame as pg
 
-alpha = 0.7
-beta  = 0.9999
 
-def initPheromone( tDimensions, tFoodPos ):
-    pheromon = np.zeros((tDimensions[0]+2,tDimensions[1]+2), dtype=np.double)
-    pheromon[tFoodPos[0]+1,tFoodPos[1]+1] = 1.
-    return pheromon
+class Pheromon:
+    """
+    """
+    def __init__(self, the_dimensions, the_food_position, the_alpha=0.7, the_beta=0.9999):
+        self.alpha = the_alpha
+        self.beta  = the_beta
+        #  We add a row of cells at the bottom, top, left, and right to facilitate edge management in vectorized form
+        self.pheromon = np.zeros((the_dimensions[0]+2, the_dimensions[1]+2), dtype=np.double)
+        self.pheromon[the_food_position[0]+1, the_food_position[1]+1] = 1.
 
-def doEvaporation( tPheromon ):
-    return beta * tPheromon
+    def do_evaporation(self, the_pos_food):
+        self.pheromon = self.beta * self.pheromon
+        self.pheromon[the_pos_food[0]+1, the_pos_food[1]+1] = 1.
 
-def mark( tPheromon, tPosition, hasWESNExists ):
-    #print(f"tPosition : {tPosition}")
-    assert(tPosition[0] >= 0)
-    assert(tPosition[1] >= 0)
-    cells = np.array([ tPheromon[tPosition[0]+1,tPosition[1]] if hasWESNExists[d.DIR_WEST ]   else 0.,
-                       tPheromon[tPosition[0]+1,tPosition[1]+2] if hasWESNExists[d.DIR_EAST ] else 0.,
-                       tPheromon[tPosition[0]+2,tPosition[1]+1] if hasWESNExists[d.DIR_SOUTH] else 0.,
-                       tPheromon[tPosition[0],tPosition[1]+1] if hasWESNExists[d.DIR_NORTH]   else 0. ], dtype=np.double )
-    pheromones = np.maximum(cells, 0.)
-    tPheromon[tPosition[0]+1,tPosition[1]+1] = alpha * np.max(pheromones) + (1-alpha)*0.25*(pheromones.sum())
+    def mark(self, the_position, has_WESN_exits):
+        assert(the_position[0] >= 0)
+        assert(the_position[1] >= 0)
+        cells = np.array([self.pheromon[the_position[0]+1, the_position[1]] if has_WESN_exits[d.DIR_WEST] else 0.,
+                          self.pheromon[the_position[0]+1, the_position[1]+2] if has_WESN_exits[d.DIR_EAST] else 0.,
+                          self.pheromon[the_position[0]+2, the_position[1]+1] if has_WESN_exits[d.DIR_SOUTH] else 0.,
+                          self.pheromon[the_position[0], the_position[1]+1] if has_WESN_exits[d.DIR_NORTH] else 0.], dtype=np.double)
+        pheromones = np.maximum(cells, 0.)
+        self.pheromon[the_position[0]+1, the_position[1]+1] = self.alpha*np.max(pheromones) + (1-self.alpha)*0.25*pheromones.sum()
 
-def getColor( value ):
-    val = max(min(value,1),0)
-    return [255*(val>1.E-14), 255*val, 128.]
+    def getColor(self, i: int, j: int):
+        val = max(min(self.pheromon[i, j], 1), 0)
+        return [255*(val > 1.E-14), 255*val, 128.]
 
-def displayPheromon( screen, tPheromon ) :
-    [ [ screen.fill(getColor(tPheromon[i,j]), (8*(j-1),8*(i-1),8,8)) for j in range(1,tPheromon.shape[1]-1)] for i in range(1,tPheromon.shape[0]-1)]
+    def display(self, screen):
+        [[screen.fill(self.getColor(i, j), (8*(j-1), 8*(i-1), 8, 8)) for j in range(1, self.pheromon.shape[1]-1)] for i in range(1, self.pheromon.shape[0]-1)]
